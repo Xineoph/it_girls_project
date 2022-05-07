404f0ac56e82125dda6304170fccf2f1d10dab4a
TayaKochetova <git.kochetovata@gmail.com>
2022-05-07 19:00:29 +0300

    feature/Taisiya_1 changes wishlist screen

--- lib/screens/wishlist_screen.dart
+++ lib/screens/wishlist_screen.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 
 List<BookInfo> _books = [
-  BookInfo.title('title-0'),
-  BookInfo.title('title-1'),
-  BookInfo.title('title-2'),
-  BookInfo.title('title-3'),
-  BookInfo.title('title-4'),
-  BookInfo.title('title-5'),
-  BookInfo.title('title-6'),
-  BookInfo.title('title-7'),
-  BookInfo.title('title-8'),
-  BookInfo.title('title-9'),
-  BookInfo.title('title-10'),
-  BookInfo.title('title-11'),
-  BookInfo.title('title-12'),
-  BookInfo.title('title-13')
+  BookInfo('assets/images/bookWish1.png', 160.46),
+  BookInfo('assets/images/bookWish2.png', 157.82),
+  BookInfo('assets/images/bookWish3.png', 138.67),
+  BookInfo('assets/images/bookWish4.png', 160.91),
+  BookInfo('assets/images/bookWish5.png', 150.9),
+  BookInfo('assets/images/bookWish6.png', 157.32),
+  BookInfo('assets/images/bookWish7.png', 160.46),
+  BookInfo('assets/images/bookWish8.png', 160.46),
+  BookInfo('assets/images/bookWish9.png', 160.46),
+  BookInfo('assets/images/bookWish10.png', 160.46),
+  BookInfo('assets/images/bookWish11.png', 160.46),
+  BookInfo('assets/images/bookWish1.png', 160.46),
+  BookInfo('assets/images/bookWish2.png', 157.82),
+  BookInfo('assets/images/bookWish3.png', 138.67),
+  BookInfo('assets/images/bookWish4.png', 160.91),
 ];
 
-class WishListScreen extends StatelessWidget {
+class WishListScreen extends StatefulWidget {
   const WishListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WishListScreen> createState() => _WishListScreenState();
+}
+
+class _WishListScreenState extends State<WishListScreen> {
 
   @override
   Widget build(BuildContext context) {
@@ -34,80 +41,85 @@ class WishListScreen extends StatelessWidget {
 
   Row _titleRow() {
     return Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          const Text(
-            'Wishlist',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
-            ),
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        const Text(
+          'Wishlist',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
           ),
-          Text(
-            '   ${_books.length}',
-            style: const TextStyle(
-              fontSize: 15,
-              color: Colors.black,
-            ),
+        ),
+        Text(
+          '   ${_books.length}',
+          style: const TextStyle(
+            fontSize: 15,
+            color: Colors.black,
           ),
-        ],
-      );
+        ),
+      ],
+    );
   }
 }
 
 class _VerticalScrollingList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    if (_books.isEmpty) {
+      return const Text('no data');
+    }
+    int n = _books.length ~/ 3;
+
     return SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Container(
-          margin: const EdgeInsets.all(30),
-          child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                for (int i = 0; i < _books.length / 3; i++)
-                  Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        for (int j = 0;
-                            j < 3 && (i * 3 + j) < _books.length;
-                            j++)
-                          _BookPicture(_books[i * 3 + j])
-                      ])
-              ]),
-        ));
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(15),
+            child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  _BooksColumn(0, n),
+                  _BooksColumn(n, 2 * n),
+                  _BooksColumn(2 * n, _books.length),
+                ])));
   }
 }
 
-class _BookPicture extends StatelessWidget {
-  final BookInfo _bookInfo;
+class _BooksColumn extends StatelessWidget {
+  final int _start;
+  final int _end;
 
-  const _BookPicture(this._bookInfo);
+  const _BooksColumn(this._start, this._end);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xffeeee00),
-      height: 150.0,
-      width: 104.0,
-      //double.infinity,
-      margin: const EdgeInsets.only(bottom: 10.0),
-      alignment: Alignment.center,
-      child: Text(_bookInfo.title),
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        for (int i = _start; i < _end; i++)
+          Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              height: _books[i].height,
+              width: _books[i].width,
+              child: Image.asset(
+                _books[i].url,
+                fit: BoxFit.fill,
+              ))
+      ],
     );
   }
 }
 
 class BookInfo {
-  String title;
-  String? url;
+  String url;
+  double height;
+  double width = 104.0;
 
-  BookInfo(this.title, this.url);
-
-  BookInfo.title(this.title);
+  BookInfo(this.url, this.height);
 }
